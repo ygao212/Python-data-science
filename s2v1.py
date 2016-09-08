f35689eee5d2963d35d65aab53e98db1aa5fb9db
ygao212 <yuangao2.12@outlook.com>
2016-09-08 00:24:50 -0400

    new files

---
+++ s2v1.py
@@ -0,0 +1,14 @@
+import csv
+
+def open_with_csv(filename, d='\t'):
+	data = []
+	with open(filename, encoding='utf-8') as tsvin:
+		tie_reader = csv.reader(tsvin, delimiter = '\t')
+		for line in tie_reader:
+			data.append(line)
+		return data
+
+
+data_from_csv = open_with_csv('data.csv')
+print(data_from_csv[0])
+print(data_from_csv[1])
